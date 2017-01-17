5ad5718777f14829afcd353a5c762c12c4992ac2
joshua <jrinard@hotmail.com>
2017-01-17 14:05:52 -0800

    is done event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,21 @@
 //ROOT COMPONENT
 import { Component } from '@angular/core';
 
-//Part 1 COMPONENT ANNOTATION - determines how it APPEARS // this is our VIEW
+//Part 1 COMPONENT ANNOTATION - determines how it APPEARS // this is our VIEW // Front End
 @Component({ // decorator defines new component should have functionalities outlines in the above imported component
   selector: 'app-root', // defines the specific tag to render within.
   template: `
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
    <h3>{{currentFocus}}</h3>
-   <ul> <!-- repeater DIRECTIVE --> <!-- tasks is the array and it is assigning each iteration to currentTask temporarly -->
-     <li *ngFor="let currentTask of tasks">{{currentTask.description}} <button class="btn btn-xs" (click)="editTask()">Edit</button></li><!-- assigning li tag to a loop/repeater // button is called an event binding-->
+   <ul> <!-- ngfor is a repeater DIRECTIVE --> <!-- tasks is the array and it is assigning each iteration to currentTask temporarly -->
+     <li (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button class="btn btn-xs" (click)="editTask()">Edit</button></li><!-- assigning li tag to a loop/repeater // button is called an event binding-->
    </ul>
   </div>
   `
 })
 
-//Part 2 COMPONENT CLASS DEFINITION -- determines how it BEHAVES
+//Part 2 COMPONENT CLASS DEFINITION -- determines how it BEHAVES //Back-End
 export class AppComponent {
   currentFocus: string = 'Angular Homework'; // Dynamic value
   currentTime = new Date();
@@ -29,11 +29,18 @@ export class AppComponent {
     new Task('Add README file to last few Angular repos on GitHub')
   ];
 
+  // editTask statement must be defined in the component class
   editTask() {
-    alert("works");
+    alert("You just requested to edit a Task! ");
   }
 
-
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("Task Done");
+    } else {
+      alert("Task NOT Done");
+    }
+  }
 }
 
 
