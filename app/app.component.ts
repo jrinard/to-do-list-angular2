3d88eb0104503f5bf0191ce0a09acd8afc5e654b
joshua <jrinard@hotmail.com>
2017-01-17 16:04:17 -0800

    hide and show form

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,29 +6,32 @@ import { Component } from '@angular/core';
   selector: 'app-root', // defines the specific tag to render within.
   template: `
   <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-   <h3>{{currentFocus}}</h3>
-   <ul> <!-- [class] is a property binding--><!-- ngfor is a repeater DIRECTIVE --> <!-- tasks is the array and it is assigning each iteration to currentTask temporarly -->
-     <li [class]="priorityColor(currentTask)"
-         (click)="isDone(currentTask)"
-         *ngFor="let currentTask of tasks">{{currentTask.description}}
-         <button class="btn btn-xs" (click)="editTask(currentTask)">Edit</button></li><!-- assigning li tag to a loop/repeater // button is called an event binding-->
-   </ul>
-   <hr>
-       <div>
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-       <h3>Edit Task</h3>
-       <label>Enter Task Description:</label>
-       <input [(ngModel)]="selectedTask.description"><!-- ngModel is a event and property binding// assigning input to description-->
-        <label>Enter Task Priority (1-3):</label>
-        <br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br><!-- radio buttons require predefined values to select from. -->
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-       </div>
+    <h1>To Do List</h1>
+    <h3>{{month}}-{{day}}-{{year}}</h3>
+    <h3>{{currentFocus}}</h3>
+    <ul> <!-- [class] = property binding--><!-- ngfor = repeater DIRECTIVE --><!--(click) = event binding --><!-- tasks is the array and it is assigning each iteration to currentTask temporarly -->
+      <li [class]="priorityColor(currentTask)"
+           (click)="isDone(currentTask)"
+           *ngFor="let currentTask of tasks">{{currentTask.description}}
+           <button class="btn btn-xs" (click)="editTask(currentTask)">Edit</button>
+      </li><!-- assigning li tag to a loop/repeater // button is called an event binding-->
+    </ul>
+    <hr>
+      <div *ngIf="selectedTask"> <!-- ngIF checks selectedTask's value of null. if it is not null due to clickedTask then it shows-->
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input class="form-control" [(ngModel)]="selectedTask.description"><!-- ngModel = event and property binding// assigning input to description // provides a shortcut for two-way data binding in forms-->
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1"> 1 (Low Priority)<br><!-- radio buttons require predefined values to select from. -->
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2"> 2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3"> 3 (High Priority)<br>
+      <button class="btn btn-xs" (click)="finishedEditing()">Close Edit</button><!-- When clicked changes selectedTask to null to hide -->
       </div>
-     `
+    </div>
+    `
 })
 
 //Part 2 COMPONENT CLASS DEFINITION -- determines how it BEHAVES //Back-End
@@ -44,18 +47,22 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 3)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   // editTask statement must be defined in the component class
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
 
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
   isDone(clickedTask: Task) {
     if(clickedTask.done === true) {
-      alert("Task Done");
+      // alert("Task Done");
     } else {
-      alert("Task NOT Done");
+      // alert("Task NOT Done");
     }
   }
 
